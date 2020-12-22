0bd289c3cb6294ab39b3841b9d1b291ecd944788
ashamary97 <thomasmaryasha1997@gmail.com>
2020-12-22 16:11:38 +0530

    bonbon changed to php

--- candybars.php
+++ candybars.php
@@ -8,38 +8,38 @@
     <title>Document</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="index.html"> Bonbon</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="about.html""> About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="login.html"> LogIn</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Products
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <li><a class="dropdown-item" href="candybars.html">Candy Bars</a></li>
-                  <li><a class="dropdown-item" href="brittlecandy.html">Brittle Candy</a></li>
-                 
-                  <li><a class="dropdown-item" href="chocolates.html">Chocolates</a></li>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="index.php"> Bonbon</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="about.php"> About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="login.php"> LogIn</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Products
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <li><a class="dropdown-item" href="candybars.php">Candy Bars</a></li>
+                      <li><a class="dropdown-item" href="brittlecandy.php">Brittle Candy</a></li>
+                     
+                      <li><a class="dropdown-item" href="chocolates.php">Chocolates</a></li>
+                    </ul>
+                  </li>
                 </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+              </div>
+            </div>
+          </nav>
 
       <div class="container">
           <div class="row">
